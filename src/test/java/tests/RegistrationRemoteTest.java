8c09c6b2d2fa8211f6fdb275cd154e3113c8436e
Victoria <viktalina@gmail.com>
2025-04-25 00:50:39 +0300

    fixed link

--- src/test/java/tests/RegistrationRemoteTest.java
+++ src/test/java/tests/RegistrationRemoteTest.java
@@ -24,7 +24,7 @@ public class RegistrationRemoteTest {
     Configuration.pageLoadStrategy = "eager";
     Configuration.baseUrl = "https://demoqa.com";
     Configuration.browserSize = "1920x1080";
-    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hud";
+    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     DesiredCapabilities capabilities = new DesiredCapabilities();
     capabilities.setCapability("selenoid:options", Map.<String, Object>of(
