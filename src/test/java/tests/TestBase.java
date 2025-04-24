5d43dd8dc3d4953a197d99c4caaaf60885942377
Victoria <viktalina@gmail.com>
2025-04-25 00:06:53 +0300

    added tests

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,44 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+
+  @BeforeAll
+  static void setupConfig(){
+    Configuration.baseUrl = "https://demoqa.com";
+    Configuration.browserSize = "1920x1080";
+    Configuration.pageLoadStrategy = "eager";
+
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+            "enableVNC", true,
+            "enableVideo", true
+    ));
+    Configuration.browserCapabilities = capabilities;
+
+    SelenideLogger.addListener("allure", new AllureSelenide());
+  }
+
+  @AfterEach
+  void addAttachments(){
+    Attach.screenshotAs("Last screenshot");
+    Attach.pageSource();
+    Attach.browserConsoleLogs();
+    Attach.addVideo();
+  }
+
+  void teardown(){
+    closeWebDriver();
+  }
+}
