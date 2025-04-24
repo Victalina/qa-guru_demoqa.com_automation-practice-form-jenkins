e7b30beb51db3abd39e34ed30e078394e1f14629
Victoria <viktalina@gmail.com>
2025-04-25 01:42:37 +0300

    added BeforeEach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,6 +6,7 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -20,15 +21,17 @@ public class TestBase {
     Configuration.browserSize = "1920x1080";
     Configuration.pageLoadStrategy = "eager";
     Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    SelenideLogger.addListener("allure", new AllureSelenide());
+  }
 
+  @BeforeEach
+  void setupVideo(){
     DesiredCapabilities capabilities = new DesiredCapabilities();
     capabilities.setCapability("selenoid:options", Map.<String, Object>of(
             "enableVNC", true,
             "enableVideo", true
     ));
     Configuration.browserCapabilities = capabilities;
-
-    SelenideLogger.addListener("allure", new AllureSelenide());
   }
 
   @AfterEach
